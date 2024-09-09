23f354ee4affc8f91feea83f77ed0dbadebc751c
mohamed <medd.eddahmani@gmail.com>
2024-09-09 19:50:45 +0200

    modifier le project

---
+++ day3/08_Strings/challenge04.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    char chat1[50] ;
+    char chat2[50] ;
+
+    for (int i = 0 ; i < chat1[i] != '\0' ; i++){
+        if (strcasecmp(chat1[i] , chat2[i]))
+        {
+            /* code */
+        }
+        
+    }
+
+    return 0 ;
+}
--- day3/mini-projet/functions.h
+++ day3/mini-projet/functions.h
@@ -4,9 +4,7 @@ void AjouterContact(char nom[200][50], char Numero[200][50], char email[200][50]
 
 void ModifierContact(char nom[200][50], char Numero[200][50], char email[200][50] , int CC);
 
-
 void SupprimerContact(char nom[200][50], char Numero[200][50], char email[200][50] , int *CC);
-
 
 void afficherContact(char nom[200][50], char Numero[200][50], char email[200][50] , int CC);
 
